0082efe032ee7f7b24ae4566d2a8d32107969d81
Rohan Karamel <rak218@scarletmail.rutgers.edu>
2025-06-13 02:27:32 -0400

    Initial Commit

---
+++ MandelbrotGPU/MandelbrotGPUApp.swift
@@ -0,0 +1,17 @@
+//
+//  MandelbrotGPUApp.swift
+//  MandelbrotGPU
+//
+//  Created by Rohan Karamel on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct MandelbrotGPUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MandelbrotGPUTests/MandelbrotGPUTests.swift
@@ -0,0 +1,17 @@
+//
+//  MandelbrotGPUTests.swift
+//  MandelbrotGPUTests
+//
+//  Created by Rohan Karamel on 6/13/25.
+//
+
+import Testing
+@testable import MandelbrotGPU
+
+struct MandelbrotGPUTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
